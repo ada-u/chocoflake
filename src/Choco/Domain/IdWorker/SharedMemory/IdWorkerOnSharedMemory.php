fb229bf369442804e2c869c0124da7b878ea4446
k2tzumi <katzumi+github@gmail.com>
2021-07-22 17:58:50 +0900

    :recycle: Refactoring to avoid duplicate IDs even when a process forks.

--- src/Choco/Domain/IdWorker/SharedMemory/IdWorkerOnSharedMemory.php
+++ src/Choco/Domain/IdWorker/SharedMemory/IdWorkerOnSharedMemory.php
@@ -29,23 +29,22 @@ class IdWorkerOnSharedMemory extends AbstractIdWorker implements IdWorkerInterfa
     const SHM_KEY = 12345;
 
     /**
-     * Key name of shared memory segment
-     * @fixme refactor
-     */
-    const SHM_SEQUENCE = 54321;
-
-    /**
      * @param IdConfig $config
      * @param RegionId $regionId
      * @param ServerId $serverId
-     * @param int $semaphoreId
+     * @param int|null $semaphoreId
      */
-    public function __construct(IdConfig $config, RegionId $regionId, ServerId $serverId, $semaphoreId = 45454)
+    public function __construct(IdConfig $config, RegionId $regionId, ServerId $serverId, $semaphoreId = null)
     {
         $this->config = $config;
         $this->regionId = $regionId;
         $this->serverId = $serverId;
-        $this->semaphoreId = $semaphoreId;
+        if ($semaphoreId) {
+            $this->semaphoreId = $semaphoreId;
+        } else {
+            $this->semaphoreId = ftok(__FILE__, chr(4));
+        }
+        // Calculate the size of shared memory.
     }
 
     /**
@@ -53,8 +52,6 @@ class IdWorkerOnSharedMemory extends AbstractIdWorker implements IdWorkerInterfa
      */
     public function generate()
     {
-        $timestamp = $this->generateTimestamp();
-
         // Acquire semaphore
         $semaphore = sem_get($this->semaphoreId);
         sem_acquire($semaphore);
@@ -64,32 +61,68 @@ class IdWorkerOnSharedMemory extends AbstractIdWorker implements IdWorkerInterfa
 
         $sequence = 0;
 
-        if (! is_null($this->lastTimestamp) && $timestamp->equals($this->lastTimestamp)) {
+        $timestamp = $this->generateTimestamp();
+        if (shm_has_var($memory, $timestamp->getValue())) {
             // Get
-            $sequence = (shm_get_var($memory, self::SHM_SEQUENCE) + 1) & $this->config->getSequenceMask();
+            $sequence = (shm_get_var($memory, $timestamp->getValue()) + 1) & $this->config->getSequenceMask();
 
-            // Increment sequence
-            shm_put_var($memory, self::SHM_SEQUENCE, $sequence);
-
-            if ($sequence === 0) {
-                usleep(1000);
-                $timestamp = $this->generateTimestamp();
+            if ($sequence > 0) {
+                // Increment sequence
+                shm_put_var($memory, $timestamp->getValue(), $sequence);
+            } else {
+                // Sequence overflowed, rerun
+                usleep(1);
+                shm_detach($memory);
+                sem_release($semaphore);
+                return $this->generate();
             }
         } else {
-            // Reset sequence if timestamp is different from last one.
             $sequence = 0;
-            shm_put_var($memory, self::SHM_SEQUENCE, $sequence);
+            // Reset sequence if timestamp is different from last one.
+            shm_put_var($memory, $timestamp->getValue(), $sequence);
         }
 
-        // Detach shared memory
+        try {
+            return new IdValue(
+                $timestamp,
+                $this->regionId,
+                $this->serverId,
+                $sequence,
+                $this->calculate($timestamp, $this->regionId, $this->serverId, $sequence)
+            );
+        } finally {
+            if ($this->lastTimestamp && !$timestamp->equals($this->lastTimestamp)) {
+                // Remove the previous shared memory variable.
+                if ($this->lastTimestamp && shm_has_var($memory, $this->lastTimestamp->getValue())) {
+                    shm_remove_var($memory, $this->lastTimestamp->getValue());
+                }
+            }
+
+            // Detach shared memory
+            shm_detach($memory);
+
+            // Update lastTimestamp
+            $this->lastTimestamp = $timestamp;
+
+            // Release semaphore
+            sem_release($semaphore);
+        }
+    }
+
+    /**
+     * Clear all data in shared memory.
+     * Call this when shared memory is not freed for some reason and you get an `shm_put_var(): not enough shared memory left` (E_WARNING level) warning
+     */
+    public function clear() {
+        $memory = shm_attach(self::SHM_KEY);
+        shm_remove($memory);
+    }
+
+    function __destruct() {
+        $memory = shm_attach(self::SHM_KEY);
+        if ($this->lastTimestamp && shm_has_var($memory, $this->lastTimestamp->getValue())) {
+            shm_remove_var($memory, $this->lastTimestamp->getValue());
+        }
         shm_detach($memory);
-
-        // Release semaphore
-        sem_release($semaphore);
-
-        // Update lastTimestamp
-        $this->lastTimestamp = $timestamp;
-
-        return new IdValue($timestamp, $this->regionId, $this->serverId, $sequence, $this->calculate($timestamp, $this->regionId, $this->serverId, $sequence));
     }
 }
