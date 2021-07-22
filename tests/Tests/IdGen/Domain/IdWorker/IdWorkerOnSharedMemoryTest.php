65dbd24eb596b18179277fa0581e81d65011a2d9
k2tzumi <katzumi+github@gmail.com>
2021-07-22 11:08:45 +0900

    :white_check_mark: Add a test case for id duplication.

--- tests/Tests/IdGen/Domain/IdWorker/IdWorkerOnSharedMemoryTest.php
+++ tests/Tests/IdGen/Domain/IdWorker/IdWorkerOnSharedMemoryTest.php
@@ -44,4 +44,84 @@ class IdWorkerOnSharedMemoryTest extends PHPUnit_Framework_TestCase
         $intValue = $this->idWorker->write($id);
         $this->assertEquals($id, $this->idWorker->read($intValue));
     }
+
+    /**
+     * @test
+     */
+    public function createIdValueWithoutDuplication()
+    {
+        $ids = array();
+        $expectedCount = 1000;
+
+        for ($i = 0; $i < $expectedCount; $i++) {
+            $ids[] = $this->idWorker->generate();
+        }
+
+        // unique ids
+        $actual = array_values(array_unique($ids));
+
+        $this->assertCount($expectedCount, $actual);
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function createIdValueWithoutDuplicationUnderProcessForks()
+    {
+        $pids = array();
+        $loopCount = 100;
+        $forkCount = 10;
+
+        // Prefix the temporary file for the result output
+        $sharedFilePrefix = tempnam('/var/tmp', get_class($this));
+
+        for ($i = 0; $i < $forkCount; $i++) {
+            // process fork
+            $pid = pcntl_fork();
+            if ($pid == -1) {
+                $this->fail('Failed to fork the process.');
+                break;
+            } else {
+                if ($pid) {
+                    // For the parent process
+                    $pids[] = $pid;
+                } else {
+                    // For child processes
+                    $sharedFile = $sharedFilePrefix . getmypid();
+                    $ids = array();
+                    for ($j = 0; $j < $loopCount; $j++) {
+                        $ids[] = $this->idWorker->generate();
+                    }
+                    // Output the result to a temporary file
+                    file_put_contents($sharedFile, serialize($ids));
+
+                    // Terminate child process
+                    exit;
+                }
+            }
+        }
+        // Waiting for the process to exit
+        $idsArray = array();
+        foreach ($pids as $pid) {
+            pcntl_waitpid($pid, $status);
+
+            // Get the result output of each process.
+            $sharedFile = $sharedFilePrefix . $pid;
+            $idsArray[] = unserialize(file_get_contents($sharedFile));
+
+            // Delete temporary files
+            unlink($sharedFile);
+        }
+
+        // flatten ids
+        $ids = array();
+        array_walk_recursive($idsArray, function($e) use (&$ids) { $ids[] = $e; });
+
+        // unique ids
+        $actual = array_values(array_unique($ids));
+
+        $this->assertCount($loopCount * $forkCount, $actual);
+    }
 }
